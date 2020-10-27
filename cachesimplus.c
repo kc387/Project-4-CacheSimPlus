60cce9f4be1eadef8e4cd77c0a3322e220a7e998
Anshu Dwibhashi <ad353@duke.edu>
2020-10-27 10:05:29 -0400

    Make it clear students have to write two of the programs from scratch

--- cachesimplus.c
+++ cachesimplus.c
@@ -1,3 +1,5 @@
 // No starter code here
 // You've already written virt2phys.c and cachesim.c
 // You have all the skills required to put them together.
+
+// TODO: Everything
--- virt2phys.c
+++ virt2phys.c
@@ -1,2 +1,4 @@
 // No starter code here
 // At this point in ECE/CS 250, you should be able to write a basic C I/O program from scratch.
+
+// TODO: Everything
