91d56636954290acd128ab70540bcbc9ac871bd7
Kathleen Chen <kathleen.chen686@duke.edu>
2020-11-13 18:44:19 -0500

    set you data

--- cachesim.c
+++ cachesim.c
@@ -130,6 +130,7 @@ int main (int argc, char* argv[]) {
 						unsigned char* d = mycache[index][i] -> data;
 						unsigned char* buff = (unsigned char*) malloc(sizeof(unsigned char) * blockSize);
 						read_from_memory(buff, (currAddress-offset), blockSize);
+						mycache[index][i] -> data = buff;
 						for(int j = offset; j < (offset + accessSize); j ++){
 							printf("%02hhx", d[j]);
 						}
@@ -164,7 +165,7 @@ int main (int argc, char* argv[]) {
 
 					unsigned char* buff = (unsigned char*) malloc(sizeof(unsigned char) * blockSize);
 					read_from_memory(buff, (currAddress-offset), blockSize);
-
+					mycache[index][ilowest] -> data = buff;
 					for(int j = offset; j < (offset + accessSize); j ++){
 						printf("%02hhx", mycache[index][ilowest] -> data[j]);
 					}
@@ -215,6 +216,7 @@ int main (int argc, char* argv[]) {
 						mycache[index][i] -> valid = 1;
 						unsigned char* buff = (unsigned char*) malloc(sizeof(unsigned char) * blockSize);
 						read_from_memory(buff, (currAddress-offset), blockSize);
+						mycache[index][i] -> data = buff;
 						for(int j = offset; j < (offset + accessSize); j ++){
 							mycache[index][i] -> data[j] = data_buffer[j-offset];
 						}
@@ -248,6 +250,7 @@ int main (int argc, char* argv[]) {
 					}
 					unsigned char* buff = (unsigned char*) malloc(sizeof(unsigned char) * blockSize);
 					read_from_memory(buff, (currAddress-offset), blockSize);
+					mycache[index][ilowest] -> data = buff;
 					mycache[index][ilowest] -> tag = tag;
 					for(int j = offset; j < (offset + accessSize); j ++){
 						mycache[index][ilowest] -> data[j] = data_buffer[j-offset];
